b54be6e5694c6103cda094cddb91690e8b09e604
DAMI KOMOLAFE <damik94@gmail.com>
2020-09-30 23:02:30 -0400

    day 1 commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Home from './Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h1>Hello Programmers</h1>
-      <Header/>
+      <Switch>
+        <Route path="/checkout">
+          <Header/>
+  
+        </Route>
+        <Route path="/">
+          <Header/>
+          <Home/>
+        </Route>
+      </Switch>
+      
     </div>
+
+    </Router>
+
+    
+    
   );
 }
 
